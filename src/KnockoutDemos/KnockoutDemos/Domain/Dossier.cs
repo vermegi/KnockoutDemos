494a0ad5941295e4c6476449b790f7710dbf7ba6
Gitte <gitte.vermeiren@gmail.com>
2012-07-14 13:16:04 +0200

    added the DetailLink

--- src/KnockoutDemos/KnockoutDemos/Domain/Dossier.cs
+++ src/KnockoutDemos/KnockoutDemos/Domain/Dossier.cs
@@ -7,5 +7,10 @@ namespace KnockoutDemos.Domain
         public string OwnerLastName { get; set; }
 
         public string OwnerFirstName { get; set; }
+
+        public string DetailLink
+        {
+            get { return string.Format("Dossier/Detail/{0}", DossierNumber); }
+        }
     }
 }
